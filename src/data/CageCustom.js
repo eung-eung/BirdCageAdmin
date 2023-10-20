aee5e2fae31cccc7f29b944dc202f4959eceff46
eung-eung <boyvip5231@gmail.com>
2023-10-20 12:05:41 +0700

    data custom cage

---
+++ src/data/CageCustom.js
@@ -0,0 +1,68 @@
+export const CageCustom = [
+    {
+        id: 1,
+        name: '',
+        length: 30,
+        width: 30,
+        height: 30,
+        createDate: "2023/10/20",
+        door: {
+            id: 1,
+            name: "Door 20/10",
+            imagePath: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHfA3fEUahY76ocv5Bfn-BPSw7pVG21C6vd_jlqrt1QiEO0aCivjjmGu0RsPJGdIwxImg&usqp=CAU"
+
+        },
+        spoke: {
+            id: 2,
+            name: "Spoke frame 20/10",
+            imagePath: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHfA3fEUahY76ocv5Bfn-BPSw7pVG21C6vd_jlqrt1QiEO0aCivjjmGu0RsPJGdIwxImg&usqp=CAU"
+
+        },
+        base: {
+            id: 3,
+            name: "Base Rose Frame 20/10",
+            imagePath: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHfA3fEUahY76ocv5Bfn-BPSw7pVG21C6vd_jlqrt1QiEO0aCivjjmGu0RsPJGdIwxImg&usqp=CAU"
+
+        },
+        roof: {
+            id: 4,
+            name: "Roof Rose Bundle 20/10",
+            imagePath: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHfA3fEUahY76ocv5Bfn-BPSw7pVG21C6vd_jlqrt1QiEO0aCivjjmGu0RsPJGdIwxImg&usqp=CAU"
+
+        },
+        status: "Pending_123"
+    },
+    {
+        id: 2,
+        name: '',
+        length: 30,
+        width: 30,
+        height: 30,
+        createDate: "2023/10/20",
+        door: {
+            id: 1,
+            name: "Door 20/10",
+            imagePath: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHfA3fEUahY76ocv5Bfn-BPSw7pVG21C6vd_jlqrt1QiEO0aCivjjmGu0RsPJGdIwxImg&usqp=CAU"
+
+        },
+        spoke: {
+            id: 2,
+            name: "Spoke frame 20/10",
+            imagePath: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHfA3fEUahY76ocv5Bfn-BPSw7pVG21C6vd_jlqrt1QiEO0aCivjjmGu0RsPJGdIwxImg&usqp=CAU"
+
+        },
+        base: {
+            id: 3,
+            name: "Base Rose Frame 20/10",
+            imagePath: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHfA3fEUahY76ocv5Bfn-BPSw7pVG21C6vd_jlqrt1QiEO0aCivjjmGu0RsPJGdIwxImg&usqp=CAU"
+
+        },
+        roof: {
+            id: 4,
+            name: "Roof Rose Bundle 20/10",
+            imagePath: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHfA3fEUahY76ocv5Bfn-BPSw7pVG21C6vd_jlqrt1QiEO0aCivjjmGu0RsPJGdIwxImg&usqp=CAU"
+
+        },
+        status: "Pending_456"
+    }
+]
