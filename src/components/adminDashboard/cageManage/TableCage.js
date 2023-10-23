e1df8cdcfcb2e88cc639f70ba7c0351c1cf5c507
Trang <ngotrang2508.forwork@gmail.com>
2023-10-23 19:26:55 +0700

    fix table

--- src/components/adminDashboard/cageManage/TableCage.js
+++ src/components/adminDashboard/cageManage/TableCage.js
@@ -1,12 +1,48 @@
 import React, { useEffect, useState } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import { Products_Cage } from '../../../data/Cages';
+import { Button, Dialog, DialogContent, DialogTitle, Typography } from '@mui/material'; // Import the necessary components
 
-const columns = [
+
+
+
+
+
+export default function TableCage() {
+    const getRowId = (row) => row._id;
+    const [descriptionDialogOpen, setDescriptionDialogOpen] = useState(false);
+    const [imageDialogOpen, setImageDialogOpen] = useState(false);
+    const [selectedRow, setSelectedRow] = useState(null);
+
+    const openDescriptionDialog = (row) => {
+        setSelectedRow(row);
+        setDescriptionDialogOpen(true);
+      };
+    
+      const openImageDialog = (row) => {
+        setSelectedRow(row);
+        setImageDialogOpen(true);
+      };
+    
+      const closeDialogs = () => {
+        setSelectedRow(null);
+        setDescriptionDialogOpen(false);
+        setImageDialogOpen(false);
+      };
+    
+
+  const columns = [
     { field: '_id', headerName: 'Id', width: 100 },
-    { field: 'name', headerName: 'Name', width: 500},
+    { field: 'name', headerName: 'Name', width: 700},
     { field: 'price', headerName: 'Price', width: 100},
-    { field: 'imagePath', headerName: 'ImageURL', width: 300},
+    {
+        field: 'imagePath',
+        headerName: 'Image',
+        width: 150,
+        renderCell: (params) => (
+          <Button variant="text" onClick={() => openImageDialog(params.row)}>View</Button>
+        ),
+      },
     { field: 'width', headerName: 'Width', width: 100},
     { field: 'length', headerName: 'Length', width: 100},
     { field: 'height', headerName: 'Height', width: 100},
@@ -14,34 +50,16 @@ const columns = [
     { field: 'createDate', headerName: 'Create Date', width: 100},
     { field: 'status', headerName: 'Status', width: 100},
     { field: 'rating', headerName: 'Rating', width: 100},
-    { field: 'description', headerName: 'Description', width: 500},
+    {
+        field: 'description',
+        headerName: 'Description',
+        width: 150,
+        renderCell: (params) => (
+            <Button variant="text" onClick={() => openDescriptionDialog(params.row)}>View</Button>
+        ),
+      },
    
 ];
-
-
-
-export default function TableCage() {
-//     const [img, setImg] = useState([]);
-
-// // Format the image data once when the component mounts or when product.images changes
-// useEffect(() => {
-//     if (Products_Cage && Products_Cage.images) {
-//       const regx = /:\[\d{3},\d{3}]/g;
-//       const regxQuotes = /(\"{|\\|}")/g;
-//       const regxCurlyBraces = /(\{)/g;
-//       const regxCurlyBraces2 = /(\})/g;
-  
-//       const formattedImages = Products_Cage.images
-//         .replace(regx, '')
-//         .replace(regxQuotes, '[')
-//         .replace(regxCurlyBraces2, ']')
-//         .replace(regxCurlyBraces, '[');
-  
-//       setImg(JSON.parse(formattedImages));
-//     }
-    
-//   }, [Products_Cage]);
-const getRowId = (row) => row._id;
     return (
         <div style={{ height: 400, marginLeft: "300px", marginTop: "30px" }}>
             <DataGrid
@@ -56,6 +74,26 @@ const getRowId = (row) => row._id;
                 pageSizeOptions={[5, 10]}
                 checkboxSelection
             />
+            {/* Description Dialog */}
+      <Dialog open={descriptionDialogOpen} onClose={closeDialogs}>
+        <DialogTitle>{selectedRow && selectedRow.name}</DialogTitle>
+        <DialogContent>
+          <Typography>{selectedRow && selectedRow.description}</Typography>
+        </DialogContent>
+      </Dialog>
+
+      {/* Image Dialog */}
+      <Dialog open={imageDialogOpen} onClose={closeDialogs}>
+        <DialogTitle>{selectedRow && selectedRow.name}</DialogTitle>
+        <DialogContent>
+        <img
+      src={selectedRow && selectedRow.imagePath}
+      alt="Product Image"
+      style={{ width: '100%', height: '300px', objectFit: 'contain' }}
+    />
+          <Typography>{selectedRow && selectedRow.imagePath}</Typography>
+        </DialogContent>
+      </Dialog>
         </div>
     );
 }
