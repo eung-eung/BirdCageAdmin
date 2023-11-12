bdf8146b6effb4a12032c5aa45ce05e502aa8e83
eung-eung <boyvip5231@gmail.com>
2023-11-13 00:28:16 +0700

    fix dialog

--- src/components/adminDashboard/cageManage/DialogViewDetail.js
+++ src/components/adminDashboard/cageManage/DialogViewDetail.js
@@ -42,23 +42,23 @@ export default function DialogViewDetail({ open, onClose, selectedData }) {
   return (
     <div>
       <Dialog width={1000} open={open} onClose={onClose}>
-        <section class="text-gray-600 body-font">
-          <div class="container px-2 py-12 mx-auto">
-            <div class="text-center">
-              <h1 class="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4">{selectedData && selectedData.name}</h1>
-              <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-2">
-                <div class="mx-auto max-w-2xl py-0 mb-3 pb-0 sm:py-0 lg:max-w-none lg:py-0">
+        <section className="text-gray-600 body-font">
+          <div className="container px-2 py-12 mx-auto">
+            <div className="text-center">
+              <h1 className="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4">{selectedData && selectedData.name}</h1>
+              <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-2">
+                <div className="mx-auto max-w-2xl py-0 mb-3 pb-0 sm:py-0 lg:max-w-none lg:py-0">
                   <div>
-                    <div class="relative h-full w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
-                      <img src={selectedData && selectedData.imagePath} alt='Main image' class="h-full w-full object-contain object-center" />
+                    <div className="relative h-full w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+                      <img src={selectedData && selectedData.imagePath} alt='Main image' className="h-full w-full object-contain object-center" />
                     </div>
                   </div>
                   {imgCage.length > 0 && (
-                    <h2 class="text-2xl font-bold text-gray-900 mb-2 mt-4">Extra images</h2>
+                    <h2 className="text-2xl font-bold text-gray-900 mb-2 mt-4">Extra images</h2>
                   )}
 
                   <div
-                    class={`
+                    className={`
     mt-2 space-y-2
     lg:grid lg:gap-x-6 lg:space-y-0
     ${imgCage.length === 2 ? 'lg:grid-cols-2' : ''}
@@ -67,9 +67,9 @@ export default function DialogViewDetail({ open, onClose, selectedData }) {
   `}
                   >
                     {imgCage.map((image, index) => (
-                      <div class="group relative" key={index}>
-                        <div class="relative h-40 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-32">
-                          <img src={image} alt={`Image ${index}`} class="h-full w-full object-contain object-center mx-auto" />
+                      <div className="group relative" key={index}>
+                        <div className="relative h-40 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-32">
+                          <img src={image} alt={`Image ${index}`} className="h-full w-full object-contain object-center mx-auto" />
                         </div>
                       </div>
                     ))}
@@ -79,7 +79,7 @@ export default function DialogViewDetail({ open, onClose, selectedData }) {
               </div>
             </div>
 
-            <div class="flex flex-wrap lg:w-4/5 mt-0 sm:mx-auto sm:mb-2 -mx-2">
+            <div className="flex flex-wrap lg:w-4/5 mt-0 sm:mx-auto sm:mb-2 -mx-2">
               {/* <div class="p-2 sm:w-full w-full">
                 <div class="bg-gray-100 rounded flex p-4 h-full items-center">
                   <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
@@ -92,116 +92,116 @@ export default function DialogViewDetail({ open, onClose, selectedData }) {
                   </span>
                 </div>
               </div> */}
-              <div class="p-2 sm:w-1/2 w-full">
-                <div class="bg-gray-100 rounded flex p-4 h-full items-center">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+              <div className="p-2 sm:w-1/2 w-full">
+                <div className="bg-gray-100 rounded flex p-4 h-full items-center">
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
                     <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
                     <path d="M22 4L12 14.01l-3-3"></path>
                   </svg>
-                  <span class="title-font font-medium">
+                  <span className="title-font font-medium">
                     <strong>Price: </strong>
                     {selectedData && selectedData.price}
                   </span>
                 </div>
               </div>
-              <div class="p-2 sm:w-1/2 w-full">
-                <div class="bg-gray-100 rounded flex p-4 h-full items-center">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+              <div className="p-2 sm:w-1/2 w-full">
+                <div className="bg-gray-100 rounded flex p-4 h-full items-center">
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
                     <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
                     <path d="M22 4L12 14.01l-3-3"></path>
                   </svg>
-                  <span class="title-font font-medium">
+                  <span className="title-font font-medium">
                     <strong>Width: </strong>
                     {selectedData && selectedData.width}
                   </span>
                 </div>
               </div>
-              <div class="p-2 sm:w-1/2 w-full">
-                <div class="bg-gray-100 rounded flex p-4 h-full items-center">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+              <div className="p-2 sm:w-1/2 w-full">
+                <div className="bg-gray-100 rounded flex p-4 h-full items-center">
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
                     <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
                     <path d="M22 4L12 14.01l-3-3"></path>
                   </svg>
-                  <span class="title-font font-medium">
+                  <span className="title-font font-medium">
                     <strong>Length: </strong>
                     {selectedData && selectedData.length}
                   </span>
                 </div>
               </div>
-              <div class="p-2 sm:w-1/2 w-full">
-                <div class="bg-gray-100 rounded flex p-4 h-full items-center">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+              <div className="p-2 sm:w-1/2 w-full">
+                <div className="bg-gray-100 rounded flex p-4 h-full items-center">
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
                     <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
                     <path d="M22 4L12 14.01l-3-3"></path>
                   </svg>
-                  <span class="title-font font-medium">
+                  <span className="title-font font-medium">
                     <strong>Height: </strong>
                     {selectedData && selectedData.height}
                   </span>
                 </div>
               </div>
-              <div class="p-2 sm:w-1/2 w-full">
-                <div class="bg-gray-100 rounded flex p-4 h-full items-center">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+              <div className="p-2 sm:w-1/2 w-full">
+                <div className="bg-gray-100 rounded flex p-4 h-full items-center">
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
                     <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
                     <path d="M22 4L12 14.01l-3-3"></path>
                   </svg>
-                  <span class="title-font font-medium">
+                  <span className="title-font font-medium">
                     <strong>In Stock: </strong>
                     {selectedData && selectedData.inStock}
                   </span>
                 </div>
               </div>
-              <div class="p-2 sm:w-1/2 w-full">
-                <div class="bg-gray-100 rounded flex p-4 h-full items-center">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+              <div className="p-2 sm:w-1/2 w-full">
+                <div className="bg-gray-100 rounded flex p-4 h-full items-center">
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
                     <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
                     <path d="M22 4L12 14.01l-3-3"></path>
                   </svg>
-                  <span class="title-font font-medium">
+                  <span className="title-font font-medium">
                     <strong>Create Date: </strong>
                     {selectedData && selectedData.createDate}
                   </span>
                 </div>
               </div>
-              <div class="p-2 sm:w-1/2 w-full">
-                <div class="bg-gray-100 rounded flex p-4 h-full items-center">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+              <div className="p-2 sm:w-1/2 w-full">
+                <div className="bg-gray-100 rounded flex p-4 h-full items-center">
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
                     <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
                     <path d="M22 4L12 14.01l-3-3"></path>
                   </svg>
-                  <span class="title-font font-medium">
+                  <span className="title-font font-medium">
                     <strong>Status: </strong>
                     {selectedData && selectedData.status}
                   </span>
                 </div>
               </div>
-              <div class="p-2 sm:w-1/2 w-full">
-                <div class="bg-gray-100 rounded flex p-4 h-full items-center">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+              <div className="p-2 sm:w-1/2 w-full">
+                <div className="bg-gray-100 rounded flex p-4 h-full items-center">
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
                     <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
                     <path d="M22 4L12 14.01l-3-3"></path>
                   </svg>
-                  <span class="title-font font-medium">
+                  <span className="title-font font-medium">
                     <strong>Rating: </strong>
                     {selectedData && selectedData.rating}
                   </span>
                 </div>
               </div>
-              <div class="p-2 sm:w-full w-full">
-                <div class="bg-gray-100 rounded flex p-4 h-full items-center">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+              <div className="p-2 sm:w-full w-full">
+                <div className="bg-gray-100 rounded flex p-4 h-full items-center">
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
                     <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
                     <path d="M22 4L12 14.01l-3-3"></path>
                   </svg>
-                  <span class="title-font font-medium">
+                  <span className="title-font font-medium">
                     <strong>Description: </strong>
                     {selectedData && selectedData.description}
                   </span>
                 </div>
               </div>
             </div>
-            <button class="flex mx-auto mt-8 mb-0 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg" onClick={onClose}>Close</button>
+            <button className="flex mx-auto mt-8 mb-0 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg" onClick={onClose}>Close</button>
           </div>
         </section>
 
